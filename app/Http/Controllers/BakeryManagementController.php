929c9dc3ccda2364c741188f9d3c8e8c7a23a93d
unknown <bikamboanita@gmail.com>
2022-10-10 15:03:35 +0300

    first commit

---
+++ app/Http/Controllers/BakeryManagementController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BakeryManagementController extends Controller
+{
+    //define the method to be hit by the route
+
+    public function saveItem(){
+        return view('welcome');
+    }
+}
